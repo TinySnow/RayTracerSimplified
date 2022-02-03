aa1beaa26b6c3cb0539aa644bf2a45606df81e5f
TinySnow <lo.tinysnow.ol@outlook.com>
2022-02-03 21:40:05 +0800

    Added a progress indicator

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@ int main() {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height - 1; j >= 0; --j) {
+        std::cerr << "\r余下进度：" << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
             auto r = double(i) / (image_width - 1);
             auto g = double(j) / (image_height - 1);
@@ -22,4 +23,5 @@ int main() {
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+    std::cerr << "\n渲染完成。\n";
 }
