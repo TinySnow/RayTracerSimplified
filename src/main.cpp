02ef63594579402216096843ed5202f32c0addbb
TinySnow <lo.tinysnow.ol@outlook.com>
2022-02-03 21:24:06 +0800

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+    // 定义图像长和宽
+    const int image_width = 256;
+    const int image_height = 256;
+    // 重定义输出位置
+    freopen("../src/image.ppm", "w", stdout);
+    // 渲染
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = image_height - 1; j >= 0; --j) {
+        for (int i = 0; i < image_width; ++i) {
+            auto r = double(i) / (image_width - 1);
+            auto g = double(j) / (image_height - 1);
+            auto b = 0.25;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+}
